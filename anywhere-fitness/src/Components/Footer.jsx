7b4b0feefbb76c068261b77517e7d89bd7bd8b36
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2021-01-07 15:00:03 -0500

    conflict resolved

--- anywhere-fitness/src/Components/Footer.jsx
+++ anywhere-fitness/src/Components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPhone, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram, faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 import '../styles/style.css';
 import '../styles/utilities.css'
 
 const Footer = () => {
     
     return (
-
+        <>
         <section id="contact" className='bg-secondary'>
         <div className="inner">
             <section className='split'>
@@ -55,12 +56,25 @@ const Footer = () => {
                         Nashville, TN 00000<br />
                         United States of America</span>
                     </div>
-             
             </section>
         </div>
     </section>
-
+         <footer id="footer" className='bg-secondary'>
+						<div class="footer-inner">
+							<ul class="footer-icons">
+								<li><a href="#"><FontAwesomeIcon icon={faTwitter}/></a></li><span class="label">Twitter</span>
+								<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								<li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</footer>
+        </>
     )
 }
 
-export default Footer
+export default Footer;
